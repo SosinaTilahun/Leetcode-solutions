7d283f4bbe50543545053012ff0df9a68f3ed4ff
Sosina Tilahun <149195300+SosinaTilahun@users.noreply.github.com>
2026-05-11 08:59:54 -0700

    Time: 19 ms (22.76%), Space: 12.6 MB (93.5%) - LeetHub

---
+++ Python/Easy/2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def separateDigits(self, nums):
+        res = []
+
+        for num in nums:
+            for ch in str(num):
+                res.append(int(ch))
+
+        return res
