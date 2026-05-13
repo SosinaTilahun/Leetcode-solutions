29fdca1eeb2ef62f21923b0a1dcf70f054b79e3c
Sosina Tilahun <149195300+SosinaTilahun@users.noreply.github.com>
2026-05-13 12:35:13 -0700

    Time: 1 ms (10.8%), Space: 12.8 MB (8.51%) - LeetHub

---
+++ Python/Easy/0058-length-of-last-word/0058-length-of-last-word.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lengthOfLastWord(self, s):
+        s = s.strip()
+        
+        length = 0
+
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == ' ':
+                break
+            length += 1
+
+        return length
