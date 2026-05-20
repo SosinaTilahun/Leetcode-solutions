5afb64d2343d68031970ee16eb895abe0df3cd17
Sosina Tilahun <149195300+SosinaTilahun@users.noreply.github.com>
2026-05-20 05:39:08 -0700

    Time: 263 ms (46.94%), Space: 131.3 MB (48.12%) - LeetHub

---
+++ Python/Medium/0077-combinations/0077-combinations.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combine(self, n, k):
+
+        result = []
+
+        def backtrack(start, path):
+
+            if len(path) == k:
+                result.append(path[:])
+                return
+
+            for num in range(start, n + 1):
+                path.append(num)
+
+                backtrack(num + 1, path)
+
+                path.pop()
+
+        backtrack(1, [])
+
+        return result
