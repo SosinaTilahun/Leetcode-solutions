218a655929dc10eb2e841ab50f07ee66072a5efd
Sosina Tilahun <149195300+SosinaTilahun@users.noreply.github.com>
2026-05-23 10:40:18 -0700

    Time: 9 ms (49.94%), Space: 16.3 MB (30.43%) - LeetHub

---
+++ Python/Medium/0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def generateTrees(self, n):
+        """
+        :type n: int
+        :rtype: List[TreeNode]
+        """
+
+        if n == 0:
+            return []
+
+        def build(start, end):
+
+            if start > end:
+                return [None]
+
+            trees = []
+
+            for root_val in range(start, end + 1):
+
+                left_trees = build(start, root_val - 1)
+                right_trees = build(root_val + 1, end)
+
+                for left in left_trees:
+                    for right in right_trees:
+
+                        root = TreeNode(root_val)
+                        root.left = left
+                        root.right = right
+
+                        trees.append(root)
+
+            return trees
+
+        return build(1, n)
